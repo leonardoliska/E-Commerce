48fafc929c19a0cc536ed28788cd8111e5df2702
leonardoliska <leo.liska@hotmail.com>
2021-11-19 23:10:05 -0300

    Funcionalidade do valor total e quantidade de itens  no carrinho

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -92,45 +92,19 @@ function createCard(card){
     infoConteiner.appendChild(itemPrice)
     infoConteiner.appendChild(itemAdd)
 
+    // Criar eventListener do botão de adicionar ao carrinho
+    itemAdd.addEventListener('click', addToCart)
+
     // Adicionar o card à vitrine
     let vitrine = document.getElementsByClassName('vitrine')[0]
     vitrine.appendChild(cardConteiner)
-}
-
-// Função para adicionar elementos para o carrinho vazio
-function emptyCart() {
-    // Criar os elementos do carrinho sem itens
-    let emptyCartConteiner = document.createElement('div')
-    let emptyCartTitle = document.createElement('h2')
-    let emptyCartAdd = document.createElement('a')
-
-    // Adicionar texto nos elementos
-    emptyCartTitle.innerText = 'Carrinho Vazio'
-    emptyCartAdd.innerText = 'Adicionar itens'
-
-    // Adicionar classes aos elementos
-    emptyCartConteiner.classList.add('carrinho--vazio-conteiner')
-    emptyCartAdd.classList.add('carrinho--card-add')
-
-    // Adicionar elementos em seu parent
-    emptyCartConteiner.appendChild(emptyCartTitle)
-    emptyCartConteiner.appendChild(emptyCartAdd)
-
-    // Adicionar conteiner de elementos no carrinho
-    carrinho = document.getElementsByClassName('carrinho--lista')[0]
-    carrinho.appendChild(emptyCartConteiner)
-
-    // Desativa o elemento de quantidade de itens no carrinho
-    let carrinhoQuantidadeItens = document.getElementsByClassName('carrinho-preço')[0]
-    carrinhoQuantidadeItens.style.display = 'none'
-
 }
 
 // Função para adicionar card ao carrinho
 function addToCart(event) {
     // Seleciona o card clicado
     const cardID = event.target.parentNode.parentNode.id        // Seleciona o ID do card
-    const card = cards[cardID.split('-')[1]]                    // Seleciona o card com o mesmo ID
+    const card = cards[cardID.split('-')[1]]                    // Seleciona o card na lista de cards com o mesmo ID
 
     // Seleciona o carrinho
     let carrinho = document.getElementsByClassName('carrinho--lista')[0]
@@ -167,38 +141,94 @@ function addToCart(event) {
     infoConteiner.appendChild(removeCard)
 
     // Adicionar a função de remover produto
-    removeCard.addEventListener('click', function () {
-        cardConteiner.remove()
-        // Verifica se o carrinho ficou vazio após remover o item para adicionar elementos do carrinho vazio
-        if (carrinho.children.length == 0) {
-            emptyCart()
-        }
-    })
+    removeCard.addEventListener('click', removeCartItem)
 
     // Verifica se o carrinho está vazio para remover o elemento padrão
     const carrinhoVazio = carrinho.children[0].nodeName == 'DIV'
     if (carrinhoVazio) {
             carrinho.removeChild(carrinho.children[0])
+            let carrinhoTotal = document.getElementsByClassName('carrinho-total')[0]
+            carrinhoTotal.style.display = 'flex'
     }
     // Adicionar itemConteiner em carrinho
     carrinho.appendChild(cardConteiner)
 
-    // Ativa o elemento de quantidade de itens no carrinho
-    let carrinhoQuantidadeItens = document.getElementsByClassName('carrinho-preço')[0]
-    carrinhoQuantidadeItens.style.display = 'flex'
+    // Atualiza o valor total do carrinho
+    updateCartTotal()
 }
 
+// Função para remover item do carrinho
+function removeCartItem(event) {
+    // Remove o card
+    const cartItem = event.target.parentNode.parentNode
+    cartItem.remove()
 
-// Criar todos os cards base
+    // Verifica se o carrinho está vazio e remove o elemento 'carrinho-total' se estiver
+    const isCartEmpty = carrinho.children.length == 0
+    if (isCartEmpty) {
+        createEmptyCart()
+        const carrinhoTotal = document.getElementsByClassName('carrinho-total')[0]
+        carrinhoTotal.style.display = 'none'
+    }
+    // Atualiza o valor total do carrinho
+    updateCartTotal()
+}
+
+// Função para adicionar elementos para o carrinho vazio
+function createEmptyCart() {
+    // Criar os elementos do carrinho sem itens
+    let emptyCartConteiner = document.createElement('div')
+    let emptyCartTitle = document.createElement('h2')
+    let emptyCartAdd = document.createElement('a')
+
+    // Adicionar texto nos elementos
+    emptyCartTitle.innerText = 'Carrinho Vazio'
+    emptyCartAdd.innerText = 'Adicionar itens'
+
+    // Adicionar classes aos elementos
+    emptyCartConteiner.classList.add('carrinho--vazio-conteiner')
+    emptyCartAdd.classList.add('carrinho--card-add')
+
+    // Adicionar elementos em seu parent
+    emptyCartConteiner.appendChild(emptyCartTitle)
+    emptyCartConteiner.appendChild(emptyCartAdd)
+
+    // Adicionar conteiner de elementos no carrinho
+    carrinho = document.getElementsByClassName('carrinho--lista')[0]
+    carrinho.appendChild(emptyCartConteiner)
+}
+
+function updateCartTotal() {
+    // Selecionar elementos que serão atualizados
+    let quantityElement = document.getElementById('carrinho-quantity')
+    let priceElement= document.getElementById('carrinho-price')
+
+    // Receber a quantidade total de itens
+    let cartTotalQuantity = document.getElementsByClassName('carrinho--card')
+    cartTotalQuantity = cartTotalQuantity.length.toString()
+
+    // Receber o preço total dos itens
+    const cartCards = document.getElementsByClassName('carrinho--card')
+    let cartTotalPrice = 0
+    for (let i = 0; i < cartCards.length; i++) {                             // Iterar por todos os cards no carrinho
+        let cardPrice = cartCards[i].childNodes[1].childNodes[1].innerText   // Acessa o elemento que contém o valor do card
+        cardPrice = parseInt(cardPrice.split(' ')[1])                        // Transforma a string 'R$ 100' em número
+        cartTotalPrice += cardPrice
+    }
+    cartTotalPrice = `R$ ${cartTotalPrice},00`
+
+    // Atualizar os elementos de acordo com os as quantidades recebidas
+    quantityElement.innerText = cartTotalQuantity
+    priceElement.innerText = cartTotalPrice
+}
+
+// Criar os cards padrão
 for(const card of Object.values(cards)) {
     createCard(card)
 }
 
 // Criar elemento padrão do carrinho
-emptyCart()
+createEmptyCart()
 
-// Adicionar eventListener em cada botão de 'Adicionar ao carrinho'
-addButton = document.getElementsByClassName('vitrine--card-add')
-for (let i = 0; i < addButton.length; i++) {
-    addButton[i].addEventListener('click', addToCart)
-}
+
+
